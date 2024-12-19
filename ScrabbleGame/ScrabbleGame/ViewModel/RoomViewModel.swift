4435839172c62c7132afd73333ea55e50cd3738b
mayaffia <maiya.ifraimova@yandex.ru>
2024-12-20 02:30:11 +0300

    IUBOUHBOUHBO

--- ScrabbleGame/ScrabbleGame/ViewModel/RoomViewModel.swift
+++ ScrabbleGame/ScrabbleGame/ViewModel/RoomViewModel.swift
@@ -8,10 +8,13 @@ class RoomViewModel: ObservableObject {
     @Published var isRoomCreated: Bool = false
     @Published var alertMessage: String = ""
     @Published var isShowingAlert: Bool = false
+    @Published var isShowingRoomInfo: Bool = false
     @Published var isSuccess: Bool = true
-    @Published var isPlayerInRoom: Bool = false
-    
+    @Published var currentRoomId: String = ""
+    @Published var roomDetails: (playerCount: Int, timePerMove: Int, inviteCode: String)? = nil
     @Published var invitationCode: String = ""
+    @Published var isPlayerInRoom: Bool = false
+
     
      
     
@@ -26,105 +29,128 @@ class RoomViewModel: ObservableObject {
         self.authViewModel = authViewModel
     }
     
-    func joinRoomWithInvitationCode() {
-        guard !self.invitationCode.isEmpty else {
-            alertMessage = ""
+    func joinRoomByInviteCode(with inviteCode: String) {
+        guard !inviteCode.isEmpty else {
+            alertMessage = "Пожалуйста, введите корректный пригласительный код."
             isShowingAlert = true
             return
         }
-    }
-    
-    
-    func joinRandomPublicRoom() {
-        joinRandomPublicRoomAPI { result in
+        let joinRoomRequest = JoinRoomByInvitationCodeRequest(inviteCode: inviteCode)
+        joinRoomRequestAPI(joinRoomRequest: joinRoomRequest) { result in
             DispatchQueue.main.async {
                 switch result {
-                case .success(let message):
-                    self.alertMessage = message
+                case .success(let room):
+                    self.alertMessage = "Вы успешно присоединились к комнате."
                     self.isShowingAlert = true
+                    self.currentRoomId = room
                     self.isSuccess = true
+                    self.isShowingRoomInfo = true
+                    self.roomDetails = (
+                                            playerCount: 4,
+                                            timePerMove: 60,
+                                            inviteCode: inviteCode
+                                        )
                 case .failure(let error):
-                    self.alertMessage = "Не удалось присоединиться к комнате: \(error.localizedDescription)"
+                    self.alertMessage = "Не удалось присоединиться к комнате. Попробуйте еще раз."
                     self.isShowingAlert = true
                     self.isSuccess = false
                 }
             }
         }
     }
+
+
+    
+    func joinRandomPublicRoom() {
+           joinRandomPublicRoomAPI { result in
+               DispatchQueue.main.async {
+                   switch result {
+                   case .success(let message):
+                       self.alertMessage = message
+                       self.isShowingAlert = true
+                       self.isSuccess = true
+                   case .failure(let error):
+                       self.alertMessage = "Не удалось присоединиться к комнате: \(error.localizedDescription)"
+                       self.isShowingAlert = true
+                       self.isSuccess = false
+                   }
+               }
+           }
+       }
+    
     
     private func joinRandomPublicRoomAPI(completion: @escaping (Result<String, Error>) -> Void) {
-        guard let url = URL(string: "\(baseURL)/rooms/joinRandomPublic") else {
-            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        // РАСКОМЕНТИТЬ И УБРАТЬ ТОКЕН ЗАХАРЖКОЖЕННЫЙ
-        //let accessToken = authViewModel.accessToken
-        /*if let token = accessToken {
-            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        } else {
-            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Отсутствует токен доступа"])))
-            return
-        }*/
-        request.setValue("Bearer rRT2nYd9cCELM1IZPixdI7zX9YH6cj3VMaNNJT77mOY=", forHTTPHeaderField: "Authorization")
-        
-        let parameters: [String: Any] = [
-            "inviteCode": "inviteCode"
-        ]
+            guard let url = URL(string: "\(baseURL)/rooms/joinRandomPublic") else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            // РАСКОМЕНТИТЬ И УБРАТЬ ТОКЕН ЗАХАРЖКОЖЕННЫЙ
+            //let accessToken = authViewModel.accessToken
+            /*if let token = accessToken {
+                request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            } else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Отсутствует токен доступа"])))
+                return
+            }*/
+            request.setValue("Bearer rRT2nYd9cCELM1IZPixdI7zX9YH6cj3VMaNNJT77mOY=", forHTTPHeaderField: "Authorization")
+            
+            let parameters: [String: Any] = [
+                "inviteCode": "inviteCode"
+            ]
 
-        if let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) {
-            request.httpBody = httpBody
+            if let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) {
+                request.httpBody = httpBody
+            }
+
+
+    print(request)
+            print(request.allHTTPHeaderFields)
+           
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        
+                
+                guard let data = data,
+                      let responseString = String(data: data, encoding: .utf8) else {
+                    DispatchQueue.main.async {
+                        completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received from server"])))
+                    }
+                    return
+                }
+                
+                //print("rr", responseString)
+                if (responseString == """
+    {"error":true,"reason":"Database error occurred: PSQLError – Generic description to prevent accidental leakage of sensitive data. For debugging details, use String(reflecting: error)."}
+    """ || responseString == """
+    {"reason":"Database error occurred: PSQLError – Generic description to prevent accidental leakage of sensitive data. For debugging details, use String(reflecting: error).","error":true}
+    """) {
+                    DispatchQueue.main.async {
+                        self.isPlayerInRoom = true
+                        completion(.success("Вы находитесь в комнате"))
+                    }
+                
+                    return
+                }
+               // print(responseString)
+                
+                if let error = error {
+                    DispatchQueue.main.async {
+                        completion(.failure(error))
+                    }
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    completion(.success(responseString))
+                }
+            }
+            task.resume()
         }
 
-       
-        
-        print(request)
-        print(request.allHTTPHeaderFields)
-       
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-    
-            
-            guard let data = data,
-                  let responseString = String(data: data, encoding: .utf8) else {
-                DispatchQueue.main.async {
-                    completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received from server"])))
-                }
-                return
-            }
-            
-            //print("rr", responseString)
-            if (responseString == """
-{"error":true,"reason":"Database error occurred: PSQLError – Generic description to prevent accidental leakage of sensitive data. For debugging details, use `String(reflecting: error)`."}
-""" || responseString == """
-{"reason":"Database error occurred: PSQLError – Generic description to prevent accidental leakage of sensitive data. For debugging details, use `String(reflecting: error)`.","error":true}
-""") {
-                DispatchQueue.main.async {
-                    self.isPlayerInRoom = true
-                    completion(.success("Вы находитесь в комнате"))
-                }
-            
-                return
-            }
-           // print(responseString)
-            
-            if let error = error {
-                DispatchQueue.main.async {
-                    completion(.failure(error))
-                }
-                return
-            }
-            
-            DispatchQueue.main.async {
-                completion(.success(responseString))
-            }
-        }
-        task.resume()
-    }
-    
     
     func createRoom() {
         guard timePerTurn > 0, maxPlayers > 1 else {
@@ -161,17 +187,15 @@ class RoomViewModel: ObservableObject {
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
+        let accessToken = "mnT84B8ZhLPTpo5HenIYlWJUybcR1GE75P+9zyiNyJA="
+        request.setValue("Bearer mnT84B8ZhLPTpo5HenIYlWJUybcR1GE75P+9zyiNyJA=", forHTTPHeaderField: "Authorization")
         
-        request.setValue("Bearer rRT2nYd9cCELM1IZPixdI7zX9YH6cj3VMaNNJT77mOY=", forHTTPHeaderField: "Authorization")
-        // РАСКОМЕНТИТЬ И УБРАТЬ ТОКЕН ЗАХАРЖКОЖЕННЫЙ
-        /*let accessToken = authViewModel.accessToken
-        
-        if let token = accessToken {
-            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        } else {
-            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Отсутствует токен доступа"])))
-            return
-        }*/
+//        if let token = accessToken {
+//            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+//        } else {
+//            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Отсутствует токен доступа"])))
+//            return
+//        }
         
         // Encode the request body
         let jsonData = try? JSONEncoder().encode(createRoomRequest)
@@ -210,5 +234,61 @@ class RoomViewModel: ObservableObject {
         }
         task.resume()
     }
+    
+    private func joinRoomRequestAPI(joinRoomRequest: JoinRoomByInvitationCodeRequest, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let url = URL(string: "\(baseURL)/rooms/joinByInviteCode") else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Неверный URL"])))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let accessToken = authViewModel.accessToken
+        
+        if let token = accessToken {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        } else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Отсутствует токен доступа"])))
+            return
+        }
+        
+        let jsonData: Data
+        do {
+            jsonData = try JSONEncoder().encode(joinRoomRequest)
+        } catch {
+            completion(.failure(error))
+            return
+        }
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Нет данных от сервера"])))
+                }
+                return
+            }
+            
+            do {
+                let roomDTO = try JSONDecoder().decode(JoinRoomByInvitationCodeResponse.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success("Вы успешно присоединились к комнате"))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
 }
-
